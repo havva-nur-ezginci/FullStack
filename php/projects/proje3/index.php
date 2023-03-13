c0e8d42b348dd1ca888724dd3b9a659d00364385
Havva Nur EZGİNCİ <62055621+havva-nur-ezginci@users.noreply.github.com>
2023-03-14 00:09:16 +0300

    superglobals

--- php/projects/proje3/index.php
+++ php/projects/proje3/index.php
@@ -7,13 +7,14 @@
     <title>SUPERGLOBALS</title>
 </head>
 <body>
-<form action="" method="POST">
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST"enctype="multipart/form-data"><!--action bunun yerine boş da bırakılabilir -->
     <input type="text" name="firstName" placeholder="Ad">
     <input type="submit" value="Gönder">
+    <input type="file" name="resim">
     <!-- <button type="submit">Gönder</button> -->
 </form>
 
-<a href="index.php?param1=parametre_karsiligi">Anasayfaya Git -> $GET</a>
+<a href="index.php?param1=parametre_karsiligi">Anasayfaya Git => $GET</a>
     
 <?php  
 
@@ -30,7 +31,7 @@ $_ENV
 $_COOKIE
 $_SESSION */
 
-if($_SERVER["REQUEST_METHOD"]== "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     print_r($_REQUEST);
 
     $firstname = $_REQUEST['firstName'];// input un name ini yazıyoruz.
@@ -44,16 +45,27 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
       } else {
         echo $firstname;
       }
+    
+      print_r($_FILES);// resme ait bilgiler gelir.
+      /**resim: {
+name: "manzara.jpg",
+type: "image/jpeg",
+tmp_name: "/tmp/phpStWk8O",
+error: "0",
+size: "29516"
+} */
 }
-else if($_SERVER["REQUEST_METHOD"]=="GET"){
+else if($_SERVER["REQUEST_METHOD"] == "GET"){
     //print_r($_GET);
-    if(isset($_GET['param1'])){
+    if(isset($_GET['param1'])){//isset : verilen parametre array de var mı?
         echo "<hr>";//düz çizgi
         echo "Paramere : ".$_GET['param1'];
 
     }
     
 }
+
+
 
 ?>
 </body>
